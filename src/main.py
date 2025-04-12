c9da47f153be1f401b34848d1a1d65cc78c9c8aa
Junqing-Gong <77088201+Junqing-Gong@users.noreply.github.com>
2025-04-12 22:22:43 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import numpy as np
 import pickle as pk
 from tqdm import tqdm
 from collections import defaultdict
+import sklearn
 
 def preprocess(input_txt_path):
     word2id, doc_with_idx = {}, []
